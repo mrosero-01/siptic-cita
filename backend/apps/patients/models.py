4a2077f1ce2114fbd7e4e074aad2d18e31bb225b
Miguel Rosero <lenkev01@gmail.com>
2026-05-15 00:32:51 -0500

    Add basic API patient

---
+++ backend/apps/patients/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+DOCUMENT_TYPE = [ ("TI", "Tarjeta de identidad"),
+                 ("CC", "Cedula de ciudadania"),
+                 ("RC", "Registro civil"),
+                 ("PS", "Pasaporte")]
+
+# Create your models here.
+class Patient(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    document_type = models.CharField(max_length=2, choices=DOCUMENT_TYPE)
+    n_document  = models.CharField(max_length=50,unique=True)
+    phone = models.CharField(max_length=19)
+    status = models.BooleanField(default=True)
+    birth_date = models.DateField()
+    comments = models.TextField(max_length=300, blank=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.n_document} - {self.first_name} {self.last_name}"
---
+++ backend/apps/patients/serializers.py
@@ -0,0 +1,24 @@
+from rest_framework import serializers
+from .models import Patient
+
+class PatientSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Patient
+        fields = [
+            'id',
+            'first_name',
+            'last_name',
+            'document_type',
+            'n_document',
+            'phone',
+            'status',
+            'birth_date',
+            'comments',
+            'created_at',
+            'updated_at',
+        ]
+        read_only_fields = [
+            'id',
+            'created_at',
+            'updated_at',
+        ]
