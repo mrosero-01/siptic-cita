37480a1981366abef31aa68528ce981f8e6c91e8
Miguel Rosero <lenkev01@gmail.com>
2026-05-16 22:54:41 -0500

    Fix some styles and table for specialties and doctors

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -3,15 +3,18 @@ import { UpperCasePipe } from '@angular/common';
 import { SidebarComponent } from './components/sidebar/sidebar';
 import { AppointmentsComponent } from './components/appointments/appointments';
 import { PatientsComponent } from './components/patients/patients';
+import { DoctorsComponent } from './components/doctors/doctors'; // <-- 1. Importamos
+import { SpecialtiesComponent } from './components/specialties/specialties';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  // 2. Agregamos UpperCasePipe al arreglo de imports
   imports: [
     SidebarComponent, 
     AppointmentsComponent,
     PatientsComponent,
+    DoctorsComponent, // <-- 2. Registramos en los imports
+    SpecialtiesComponent,
     UpperCasePipe 
   ],
   templateUrl: './app.html',
@@ -19,7 +22,6 @@ import { PatientsComponent } from './components/patients/patients';
 })
 export class App {
   currentView = signal<string>('citas');
-  
 
   changeView(viewName: string) {
     this.currentView.set(viewName);
